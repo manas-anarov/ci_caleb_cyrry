2b0305be301a49253bb3a044307e7ea3602068a9
manas-anarov <you@example.com>
2020-08-03 14:33:46 +0600

    12 lesson not video

--- subscribe.c
+++ subscribe.c
@@ -5,6 +5,10 @@ int main(){
 	printf("Please enter a radius: ");
 	scanf("%i", &radius);
 	printf("The given radius is %i\n",radius);
+	
+
+	double area = 3.14159 * (radius * radius);
+	printf("The area of a circle with %i radius is %f\n", area);
 
 	return 0;
 }
