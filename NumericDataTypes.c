46feca690c294d7dc3f471a06a26470febac2544
manas-anarov <you@example.com>
2021-07-20 02:12:11 +0600

    23_lesson

--- NumericDataTypes.c
+++ NumericDataTypes.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main(){
 
-	int dogs=2.5e4;
+	// int dogs=2.5e4;
+	printf("How many dogs do you have?: ");
+	double dogs;
+	scanf("%lf", &dogs);
 
 
 
-	printf("%i\n" , dogs);
+	printf("%f\n%e\n%g\n" , dogs);
 
 	return 0;
 }
